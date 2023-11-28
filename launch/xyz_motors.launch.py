b7de8090f4803d9907a13fe3807820b18efd6f49
Thies Lennart Alff <lennart.alff@gmail.com>
2023-11-28 12:02:23 +0000

    added xyz launch file

---
+++ launch/xyz_motors.launch.py
@@ -0,0 +1,15 @@
+from launch_ros.actions import Node
+
+from launch import LaunchDescription
+
+
+def add_xyz_motors_node(launch_description: LaunchDescription):
+    action = Node(executable='xyz_motors', package='gantry', namespace='gantry')
+    launch_description.add_action(action)
+
+
+def generate_launch_description():
+    launch_description = LaunchDescription()
+    add_xyz_motors_node(launch_description=launch_description)
+
+    return launch_description
